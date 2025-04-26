9c550263dce9a1ed260f75b4a6d3d97f0024c586
AhmadAlaa1 <ahmadalaa114410@gmail.com>
2025-04-26 19:07:51 +0300

    Added Getters and Setters

--- src/main/java/com/project/AssetTrackingSystem/model/Asset.java
+++ src/main/java/com/project/AssetTrackingSystem/model/Asset.java
@@ -27,5 +27,44 @@ public class Asset {
         INUSE,
         MAINTAINCE
     }
-    
+
+     public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/project/AssetTrackingSystem/model/AssetAssignment.java
+++ src/main/java/com/project/AssetTrackingSystem/model/AssetAssignment.java
@@ -26,6 +26,44 @@ public class AssetAssignment {
     @JoinColumn(name = "asset_id", nullable = false)
     private Asset asset;
 
-    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getAssignedDate() {
+        return assignedDate;
+    }
+
+    public void setAssignedDate(LocalDate assignedDate) {
+        this.assignedDate = assignedDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Asset getAsset() {
+        return asset;
+    }
+
+    public void setAsset(Asset asset) {
+        this.asset = asset;
+    }
 
 }
