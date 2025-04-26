f0cbff79d041737164aa6e24fb61ee061eaaf60a
AhmadAlaa1 <ahmadalaa114410@gmail.com>
2025-04-26 23:53:56 +0300

    Added Functions getByEmail & saveEmployee

--- src/main/java/com/project/AssetTrackingSystem/service/EmployeeService.java
+++ src/main/java/com/project/AssetTrackingSystem/service/EmployeeService.java
@@ -1,7 +1,21 @@
 package com.project.AssetTrackingSystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.project.AssetTrackingSystem.model.Employee;
+import com.project.AssetTrackingSystem.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepository empRepo;
+
+    public Employee getByEmail(String email){
+        return empRepo.findByEmail(email);
+    }
     
+    public Employee saveEmployee(Employee employee){
+        return empRepo.save(employee);
+    }
 }
