98e698284ac039f4ff1b29fe593508dff47dead4
AhmadAlaa1 <ahmadalaa114410@gmail.com>
2025-04-26 23:52:40 +0300

    Added Role Getters and Setters

--- src/main/java/com/project/AssetTrackingSystem/model/Employee.java
+++ src/main/java/com/project/AssetTrackingSystem/model/Employee.java
@@ -41,8 +41,20 @@ public class Employee {
         return name;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() {
@@ -52,6 +64,11 @@ public class Employee {
     public void setEmail(String email) {
         this.email = email;
     }
+    
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
     
 }
 
