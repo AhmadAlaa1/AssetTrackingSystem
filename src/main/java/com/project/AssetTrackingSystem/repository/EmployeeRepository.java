55ccd01446ebbd170c6e1fa528da3f630565bd58
AhmadAlaa1 <ahmadalaa114410@gmail.com>
2025-04-26 23:53:35 +0300

    Added Find By Email Method

--- src/main/java/com/project/AssetTrackingSystem/repository/EmployeeRepository.java
+++ src/main/java/com/project/AssetTrackingSystem/repository/EmployeeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.AssetTrackingSystem.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
-    
+    public Employee findByEmail(String email);
 }
